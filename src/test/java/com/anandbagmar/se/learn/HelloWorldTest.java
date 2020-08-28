f3c75bb595699dd1e51e9f618989ef698e501292
Anand Bagmar <abagmar@gmail.com>
2020-08-28 11:48:37 +0530

    - Fixed version for Applitools dependency - Updated sample website to talk to one stubbed endpoint using Qontract - Updated test scenario implementation which includes setting dynamic expectation in Qontract

--- src/test/java/com/anandbagmar/se/learn/HelloWorldTest.java
+++ src/test/java/com/anandbagmar/se/learn/HelloWorldTest.java
@@ -12,14 +12,16 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.time.Duration;
 
 import static io.restassured.RestAssured.given;
 
@@ -35,12 +37,13 @@ public class HelloWorldTest extends BaseTest {
         setupBeforeMethod(appName, method, viewportSize, true, true);
     }
 
-    @Test(description = "Login with Eyes")
-    public void login() {
+    @Test(description = "Login and update user name")
+    public void loginAndUpdateUserName() {
         Eyes eyes = getEyes();
         WebDriver driver = getDriver();
 
         setExpectationInQontract(loadAndUpdateExpectationForUsers());
+
         String url = "http://localhost:3000";
         driver.get(url);
         eyes.checkWindow("onLoad");
@@ -51,39 +54,49 @@ public class HelloWorldTest extends BaseTest {
 //        ((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='email']\").style.backgroundColor=\"grey\"");
 //        ((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='email']\").style.color=\"#431\"");
         eyes.checkWindow("enteredCredentials");
+
         driver.findElement(By.cssSelector(".form-btn")).click();
-        waitFor(10);
+        explicitlyWaitFor(driver, By.cssSelector("button.btn.logout"));
         eyes.checkWindow("after login");
+
         driver.findElement(By.cssSelector("a.btn.edit")).click();
-        eyes.checkWindow("edit user id ");
+        eyes.checkWindow("edit user name");
+        driver.findElement(By.cssSelector("input[name='first_name']")).clear();
+        driver.findElement(By.cssSelector("input[name='last_name']")).clear();
+        eyes.checkWindow("clear current user details");
+        driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys("Anand");
+        driver.findElement(By.cssSelector("input[name='last_name']")).sendKeys("Bagmar");
+        eyes.checkWindow("update user name");
+        driver.findElement(By.cssSelector(".form-btn")).click();
+
+        explicitlyWaitFor(driver, By.cssSelector("button.btn.logout"));
+        eyes.checkWindow("updated list");
     }
 
-    public static void main(String[] args) {
-        HelloWorldTest helloWorldTest = new HelloWorldTest();
-        int userId = 10;
-//        helloWorldTest.setExpectationInQontract(helloWorldTest.loadAndUpdateExpectationForUserId(userId));
-        helloWorldTest.setExpectationInQontract(helloWorldTest.loadAndUpdateExpectationForUsers());
+    private void explicitlyWaitFor(WebDriver driver, By locator) {
+        long duration = Duration.ofSeconds(15).getSeconds();
+        new WebDriverWait(driver, duration).until(ExpectedConditions.elementToBeClickable(locator));
     }
 
     private JSONObject loadAndUpdateExpectationForUsers() {
-        JSONObject jsonObject = loadFromJSON("src/test/resources/getUsersExpectations.json");
+        JSONObject jsonObject = loadFromJSON("src/test/resources/_getUsers_data_/getUsers.json");
         return jsonObject;
     }
 
-    private JSONObject loadAndUpdateExpectationForUserId(int userId) {
-        JSONObject jsonObject = loadFromJSON("src/test/resources/getUserExpectations.json");
-        JSONObject httpRequest = (JSONObject) jsonObject.get("http-request");
-        String path = (String) httpRequest.get("path") + userId;
-        httpRequest.put("path", path);
-
-        JSONObject httpResponse = (JSONObject) jsonObject.get("http-response");
-        JSONObject responseBody = (JSONObject) httpResponse.get("body");
-        JSONObject responseBodyData = (JSONObject) responseBody.get("data");
-        responseBodyData.put("id", userId);
-
-        System.out.println("Updated expectations: " + jsonObject);
-        return jsonObject;
-    }
+//    private JSONObject loadAndUpdateExpectationForUserId(int userId) {
+//        JSONObject jsonObject = loadFromJSON("src/test/resources/getUserExpectations.json");
+//        JSONObject httpRequest = (JSONObject) jsonObject.get("http-request");
+//        String path = (String) httpRequest.get("path") + userId;
+//        httpRequest.put("path", path);
+//
+//        JSONObject httpResponse = (JSONObject) jsonObject.get("http-response");
+//        JSONObject responseBody = (JSONObject) httpResponse.get("body");
+//        JSONObject responseBodyData = (JSONObject) responseBody.get("data");
+//        responseBodyData.put("id", userId);
+//
+//        System.out.println("Updated expectations: " + jsonObject);
+//        return jsonObject;
+//    }
 
     private JSONObject loadFromJSON(String fileName) {
         JSONParser jsonParser = new JSONParser();
